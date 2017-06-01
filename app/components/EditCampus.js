9e4ee64466725c5e5d34a71f787ebbf636c0da58
Patrick Hoyt <ptrckhoyt120@gmail.com>
2017-06-01 11:29:07 -0400

    fixed Edit Campus form

--- app/components/EditCampus.js
+++ app/components/EditCampus.js
@@ -4,19 +4,21 @@ import {Link} from 'react-router';
 const EditCampus = function(props){
   // const handleClick = props.handleClick;
   return (
-    <div className="well col-md-10">
+    <div className="well col-md-8">
     <h4>Edit Campus Form</h4>
     <br />
     <form className="form-inline table">
-      <div className="form-group col-md-3">
+      <div className="form-group col-md-4">
         <label >Name</label>
         <input type="text" className="form-control" id="exampleInputName2" placeholder="Jane Doe" />
       </div>
-      <div className="form-group col-md-4">
+      <div className="form-group col-md-5">
         <label >Image URL</label>
         <input type="email" className="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com" />
       </div>
-      <button type="submit" className="btn btn-primary col-xs-2">Submit</button>
+
+        <button type="submit" className="btn btn-primary col-xs-2">Submit</button>
+
     </form>
     </div>
   )
